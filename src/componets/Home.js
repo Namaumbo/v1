ef26206a4f6efad41e5ca073ac302a66374ec95d
Daelo Namaumbo <bsc-53-16@cc.ac.mw>
2022-09-24 13:49:49 +0200

    v1 potifolio

---
+++ src/componets/Home.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+export default function Home() {
+  return (
+    <>
+      <div style={{ marginLeft: "5em" }}>
+        <h1
+          style={{
+            transitionDelay: "300ms",
+            color: "#53d3b9",
+            fontFamily: "monospace",
+            fontSize: "20px",
+          }}
+        >
+          <span style={{ fontSize: "100px" }}>Hi,</span>{" "}
+          <span style={{ fontWeight: "lighter",fontSize:"25px" }}>my name is</span>
+        </h1>
+        <h1
+          style={{
+            color: "#8892b0",
+            fontSize: "120px",
+            marginTop: "-40px",
+            fontWeight: "bolder",
+            marginLeft: "-6px",
+            fontFamily: "sans-serif",
+            letterSpacing: "10px",
+          }}
+        >
+          Daelo{" "}
+        </h1>
+        <h1 style={{ color: "#ccd6f6", fontSize: "70px", marginTop: "-0.4em" }}>
+          I build things for the web.
+        </h1>
+        <br />
+        <Button variant="outline-info" aria-label="phone" style={{padding:"1em", }}>Contact me!</Button>{' '}
+        <Button variant="outline-info" style={{padding:"1em", }}>Check my projects</Button>{' '}
+      </div>
+      
+    </>
+  );
+}
